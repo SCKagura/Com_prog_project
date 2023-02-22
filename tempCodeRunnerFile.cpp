a13f65aba25456e738e903bf93bfa028aa062f9b
SCKboss <=>
2023-02-22 15:57:35 +0700

    something went wrong

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,6 +3,22 @@
 #include <string> 
 using namespace std ;
 
-int main ()
+int main() 
 {
-    
+string username,usernamefile ;
+    cout << "||      ||  ||||||||||  ||          ||            ||||||           ||  ||\n" ;
+    cout << "||      ||  ||          ||          ||          ||      ||       ||  ||  ||\n" ;
+    cout << "||||||||||  ||||||||||  ||          ||          ||      ||       ||      ||\n" ;
+    cout << "||      ||  ||          ||          ||          ||      ||         ||  ||\n" ;
+    cout << "||      ||  ||||||||||  ||||||||||  ||||||||||    ||||||             || \n" ;
+    cout << "Please Enter Your Username : " ;
+    getline (cin,username) ;
+    cout << "Your Username is :" << username ; 
+    usernamefile ="C:\\Users\\boss\\Com prog project\\Com_prog_project\\Com_prog_project\\Usernameinfo\\"+username+".txt" ;
+    cout <<usernamefile ;
+    ofstream saveusername ;
+    saveusername.open(usernamefile) ;
+    saveusername <<username;
+    saveusername.close() ;
+
+}
